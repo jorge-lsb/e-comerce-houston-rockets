d695d8c8b6ace7666663572b5552af8b3bba67e9
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-05-08 23:44:25 -0300

    Adicionando  senha visive ou invisivel

---
+++ site/js/mostrar_senha.js
@@ -0,0 +1,23 @@
+function ver_senha_1() {
+    senha_fechada_1.style.fontSize = '0pt';
+    senha_aberta_1.style.fontSize = '12pt';
+    ipt_senha_1.type = 'text';
+}
+
+function esconder_senha_1() {
+    senha_fechada_1.style.fontSize = '12pt';
+    senha_aberta_1.style.fontSize = '0pt';
+    ipt_senha_1.type = 'password';
+}
+
+function ver_senha_2() {
+    senha_fechada_2.style.fontSize = '0pt';
+    senha_aberta_2.style.fontSize = '12pt';
+    ipt_senha_2.type = 'text';
+}
+
+function esconder_senha_2() {
+    senha_fechada_2.style.fontSize = '12pt';
+    senha_aberta_2.style.fontSize = '0pt';
+    ipt_senha_2.type = 'password';
+}

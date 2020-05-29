ace57df6d449159a468518506fa9e4eb007fec22
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-05-29 20:34:10 -0300

    Adicionando validação de e-mail com JS proprio

--- site/js/validacao_cadastro.js
+++ site/js/validacao_cadastro.js
@@ -1,7 +1,8 @@
 function validating_form() {
+    // VALIDADANDO SENHA
+
     var passoword = document.getElementById("ipt_senha_1").value;
     var passoword_confirm = document.getElementById("ipt_senha_2").value;
-
     var confirmed = passoword == passoword_confirm;
 
     if (!confirmed) {
@@ -9,5 +10,16 @@ function validating_form() {
         document.getElementById("registration_form").action = "";
     } else {
         document.getElementById("registration_form").action = "../pages/loja.html";
+    }
+
+    // VALIDADANDO E-MAIL
+
+    var email_validating = document.getElementById("email").value;
+    var validated_email_successfully = (email_validating.indexOf('@') > -1 && email_validating.indexOf('.') && email_validating.length > 2);
+
+    if (!validated_email_successfully) {
+        console.log('SEU E-MAIL NÃO É VALIDO');
+        alert('SEU EMAIL NÃO É VALIDO');
+        document.getElementById("registration_form").action = "";
     }
 }

1722c2065ead92f127942f50679085f18cb81cda
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-05-29 22:20:51 -0300

    reload focus no email e input focus sufixo cep

---
+++ site/js/input_sufixo.js
@@ -0,0 +1,12 @@
+function textingprefix() {
+
+    var input_sufix = document.getElementById("id_prefix");
+
+    var reverse_input = document.getElementById("id_sufix");
+
+    if (input_sufix.value.length == 5) {
+
+        document.getElementById("id_sufix").focus();
+
+    }
+}

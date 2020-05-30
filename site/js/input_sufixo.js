f3a4aba76b4d2e895574e6df291b8890d0ab20c4
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-05-29 23:58:34 -0300

    Corrigindo bug do cep

--- site/js/input_sufixo.js
+++ site/js/input_sufixo.js
@@ -2,11 +2,22 @@ function textingprefix() {
 
     var input_sufix = document.getElementById("id_prefix");
 
-    var reverse_input = document.getElementById("id_sufix");
+
 
     if (input_sufix.value.length == 5) {
 
         document.getElementById("id_sufix").focus();
 
+    }
+}
+
+
+function textingprefixreverse() {
+
+    var reverse_input = document.getElementById("id_sufix");
+
+    if (reverse_input.value == '') {
+
+        document.getElementById("id_prefix").focus();
     }
 }

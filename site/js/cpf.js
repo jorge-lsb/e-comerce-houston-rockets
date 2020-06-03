bd639feaee9ee53bf829cd5758e6f2d27bae1753
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-06-03 00:11:00 -0300

    validando cpf e refaturando funções

---
+++ site/js/cpf.js
@@ -0,0 +1,42 @@
+//arquivo funcoes_cpf.js
+// validador CPF
+// http://www.rafaelwendel.com/2011/06/funcao-javascript-para-validar-cpf/#:~:text=Essa%20fun%C3%A7%C3%A3o%20faz%20todas%20as,um%20exemplo%20de%20sua%20aplicabilidade.
+function verificarCPF(c){
+    var i;
+    s = c;
+    var c = s.substr(0,9);
+    var dv = s.substr(9,2);
+    var d1 = 0;
+    var v = false;
+ 
+    for (i = 0; i < 9; i++){
+        d1 += c.charAt(i)*(10-i);
+    }
+    if (d1 == 0){
+        alert("CPF Inválido")
+        v = true;
+        return false;
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(0) != d1){
+        alert("CPF Inválido")
+        v = true;
+        return false;
+    }
+ 
+    d1 *= 2;
+    for (i = 0; i < 9; i++){
+        d1 += c.charAt(i)*(11-i);
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(1) != d1){
+        alert("CPF Inválido")
+        v = true;
+        return false;
+    }
+    if (!v) {
+        alert(c + "CPF Válido")
+    }
+}

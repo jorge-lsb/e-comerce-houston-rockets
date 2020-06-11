0c71af320473ac9ff9e40f50dafa3f82f99cf2a7
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-06-10 23:19:22 -0300

    Adionando style e hover na loja

--- site/js/loja.js
+++ site/js/loja.js
@@ -1,22 +1,90 @@
 
+
+
 let cards1 = {
     imagem: `../img/loja-img/regata-nike-13.jpg`,
     description: `Regata Nike James Harden`,
     price: `R$ 499,99`
 }
 
-// let cards2 = {
-//     imagem: `../img/loja-img/regata-nike--branca-13.jpg`,
-//     description: `Regata Nike James Harden`,
-//     price: `R$ 499,99`
-// }
+let cards2 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
 
-// let cards3 = {
-//     imagem: `../img/loja-img/camisa-preta-houston.jpg`,
-//     description: `Regata Nike James Harden`,
-//     price: `R$ 499,99`
-// }
+let cards3 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
 
+let cards4 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards5 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards6 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards7 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards8 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards9 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards10 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards11 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+
+let cards12 = {
+    imagem: `../img/loja-img/regata-nike-13.jpg`,
+    description: `Regata Nike James Harden`,
+    price: `R$ 499,99`
+}
+for (let contador = 1; contador <= 12; contador++) {
+    let cartas = `cards${contador}`
+    gerador.innerHTML += 
+    `   <div class="card_products" id="mudanca">
+    
+        <img src="${cards2.imagem}" id="img_card">
+    
+        <p class="desc_product" id="p_desc_product">${cards1.description}</p>
+    
+        <p class="price_product" id="p_price">${cards1.price}</p>
+
+    </div>  `;
+}
 
 // for (let contador = 0; contador < 9; contador++) {
 //     let aleatorio = parseInt(Math.random() * 3) + 1;
@@ -28,12 +96,3 @@ let cards1 = {
 //     </div>`;
 // }
 
-for (let contador = 0; contador < 9; contador++) {
-    let aleatorio = parseInt(Math.random() * 3) + 1;
-    let cartas = `cards${aleatorio}`
-    gerador.innerHTML += `<div class="card_products" id="mudanca">
-    <img src="${cards1.imagem}" id="img_card">
-    <p class="desc_product" id="p_desc_product">${cards1.description}</p>
-    <p class="price_product" id="p_price">${cards1.price}</p>
-    </div>`;
-}

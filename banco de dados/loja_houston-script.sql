c8220a29ea39b154476e2103719163d658176497
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-06-20 00:07:58 -0300

    Correção de bugs

--- banco de dados/loja_houston-script.sql
+++ banco de dados/loja_houston-script.sql
@@ -3,10 +3,11 @@ id_usuario int primary key auto_increment,
 nome_usuario varchar (100) not null,
 senha_usuario varchar(255) not null,
 email_usuario varchar(100) not null,
-cpf_usuario varchar(15) not null,
-cep_usuario varchar(10) not null
+cpf_usuario varchar(15) not null
 );
 
+alter table usuario drop column cep_usuario;
 select * from usuario;
 
+truncate usuario ;
 insert into usuario values(null,'josé','14789','jose.lucas@bandtec.com.br','43627701860','09330402');
